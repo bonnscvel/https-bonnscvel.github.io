eb16792eaa8eee8fc118b52fef9c3c6241d33627
bonns <bonns@heros3.com>
2018-11-15 15:58:26 -0200

    ww
    we

--- js/main.js
+++ js/main.js
@@ -141,19 +141,6 @@
 	};
 
 
-	var testimonialCarousel = function(){
-		var owl = $('.owl-carousel-fullwidth');
-		owl.owlCarousel({
-			items: 1,
-			loop: true,
-			margin: 0,
-			responsiveClass: true,
-			nav: false,
-			dots: true,
-			smartSpeed: 800,
-			autoHeight: true,
-		});
-	};
 
 
 	var goToTop = function() {
